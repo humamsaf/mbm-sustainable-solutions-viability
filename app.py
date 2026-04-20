3ed9f6c7c2eef7b48f7f1a9d288974ef12c780d0
humamsaf <humamsaf0@gmail.com>
2026-04-20 08:50:52 +0700

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,6 @@ from __future__ import annotations
 import numpy as np
 import pandas as pd
 import plotly.express as px
-import plotly.graph_objects as go
 import streamlit as st
 
 # ============================================================================
